7b31869388b0193384606b22f4b527c62a6ba492
injae-348 <ant0593@naver.com>
2023-10-10 18:42:11 +0900

    ch3-09 half

--- pybo/views.py
+++ pybo/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from django.utils import timezone
-from django.http import HttpResponseNotAllowed
+# from django.http import HttpResponseNotAllowed
 from .models import Question
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -25,27 +28,30 @@ def detail(request, question_id):
     context = {'question':question}
     return render(request,'pybo/question_detail.html',context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question,pk=question_id)
     if request.method == 'POST':
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect('pybo:detail',question_id=question.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.')
+        form = AnswerForm()
     context = {'question':question, 'form':form}
     return render(request,'pybo/question_detail.html',context)
 
-
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
@@ -53,3 +59,32 @@ def question_create(request):
         form = QuestionForm()
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
+
+
+@login_required(login_url='common:login')
+def question_modify(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('pybo:detail',question_id=question.id)
+    if request.method == 'POST':
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('pybo:detail',question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form':form}
+    return render(request,'pybo/question_form.html',context)
+
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '삭제권한이 없습니다')
+        return redirect('pybo:detail', question_id=question.id)
+    question.delete()
+    return redirect('pybo:index')
